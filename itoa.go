9eca5373bae42624a840f16b5dfb5778fcc82747
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-09-21 18:02:54 +0300

    Itoa

--- itoa.go
+++ itoa.go
@@ -16,19 +16,3 @@ func Itoa(i int) string {
 		return res
 	}
 }
-
-// func Itoa(n int) string {
-// 	if n == 0 {
-// 		return "0"
-// 	}
-// 	res := ""
-// 	for n > 0 {
-// 		res = string(n%10+48) + res
-// 		n /= 10
-// 	}
-// 	if n < 0 {
-// 		return "-" + Itoa(-n)
-// 	} else {
-// 		return res
-// 	}
-// }
