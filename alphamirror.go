745393c8dc77c5d5ead974c128ecabcd8fe8bb7c
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-16 23:38:11 +0200

    cleaned

--- alphamirror.go
+++ alphamirror.go
@@ -4,9 +4,9 @@ func Alphamirror(s string) string {
 	res := ""
 	for _, e := range s {
 		if e >= 'A' && e <= 'Z' {
-			res += string('A' + 'Z' -e)
+			res += string('A' + 'Z' - e)
 		} else if e >= 'a' && e <= 'z' {
-			res += string('a' + 'z' -e)
+			res += string('a' + 'z' - e)
 		} else {
 			res += string(e)
 		}
